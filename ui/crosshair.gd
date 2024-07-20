4ffa50d1e9f560e94e3b61b153bc2a2ffc1f902f
sundowns <tom@smallridge.com.au>
2024-07-20 12:54:11 +0800

    coloured crosshair

--- ui/crosshair.gd
+++ ui/crosshair.gd
@@ -2,7 +2,11 @@ extends TextureRect
 
 func _ready() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CONFINED_HIDDEN)
+	ColourManager.active_colour_changed.connect(self._on_active_colour_changed)
 
 func _process(_delta) -> void:
 	var mouse_pos: Vector2  = get_viewport().get_mouse_position()
 	global_position = mouse_pos - (size*scale)/2
+
+func _on_active_colour_changed(new_active_colour: Color) -> void:
+	self_modulate = new_active_colour
