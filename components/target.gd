a9d1bb17e2385e099682eb811139b35728a3a476
sundowns <tom@smallridge.com.au>
2024-07-20 10:58:59 +0800

    basic colouring and handling of coloured materials

---
+++ components/target.gd
@@ -0,0 +1,10 @@
+extends CharacterBody3D
+
+@export var colour: Color = Color.RED
+@onready var mesh: MeshInstance3D = $targetA2/targetA
+
+func _ready() -> void:
+	update_mesh()
+
+func update_mesh() -> void:
+	mesh.set_surface_override_material(0, ColourManager.get_material(colour))
---
+++ src/ColourManager.gd
@@ -0,0 +1,36 @@
+extends Node
+
+# TODO: Probably replace these with less offensive versions of each (i.e. not maxed out rgb vals)
+var colours: Array[Color] = [
+	# single colours
+	Color.RED, 
+	Color.BLUE,
+	Color.GREEN,
+	# combos of two
+	Color.PURPLE,
+	Color.YELLOW,
+	Color.CYAN,
+	# Three
+	Color.WHITE,
+	# None
+	Color.BLACK
+]
+
+var base_material: BaseMaterial3D = preload("res://materials/base_target_colour.tres")
+var materials: Dictionary = {}
+
+func _ready():
+	generate_materials()
+
+func generate_materials() -> void:
+	for colour in colours:
+		var coloured_material: BaseMaterial3D = base_material.duplicate()
+		coloured_material.albedo_color = colour
+		materials[colour] = coloured_material
+
+func get_material(colour: Color) -> BaseMaterial3D:
+	if materials.has(colour):
+		return materials[colour]
+	else:
+		push_error("Attempted to fetch material for unregistered colour: ", colour)
+		return base_material
